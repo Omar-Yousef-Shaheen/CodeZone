9f7f8cb15123c48f5f773e3a4e2f3d48d45a4dc0
Omar Yousef <omaryousef6696@gmail.com>
2026-09-03 05:05:00 +0300

    icon customiz

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -13,20 +13,26 @@ import {
   Search,
 } from "lucide-react";
 import type { ReactElement } from "react";
-import { FaCss3Alt, FaGithub, FaHtml5, FaJs, FaLinkedinIn, FaShopify, FaWordpress } from "react-icons/fa6";
-import { SiElementor, SiWoocommerce } from "react-icons/si";
+import { FaGithub, FaLinkedinIn, FaShopify, FaWordpress } from "react-icons/fa6";
+import wordpressIcon from "../assets/icons/icons8-wordpress-94.png";
+import htmlIcon from "../assets/icons/icons8-html-96.png";
+import cssIcon from "../assets/icons/icons8-css-96.png";
+import javascriptIcon from "../assets/icons/icons8-js-96.png";
+import shopifyIcon from "../assets/icons/icons8-shopify-96.png";
+import woocommerceIcon from "../assets/icons/icons8-woocommerce-logo-96.png";
+import elementorIcon from "../assets/icons/icons8-elementor-96.png";
 
 type IconProps = { className?: string };
 
 export function SkillIcon({ name, className = "size-4" }: IconProps & { name: string }) {
   const icons: Record<string, ReactElement> = {
-    html: <FaHtml5 className={className} />,
-    css: <FaCss3Alt className={className} />,
-    javascript: <FaJs className={className} />,
-    wordpress: <FaWordpress className={className} />,
-    shopify: <FaShopify className={className} />,
-    woocommerce: <SiWoocommerce className={className} />,
-    elementor: <SiElementor className={className} />,
+    html: <img src={htmlIcon} alt="" aria-hidden="true" className={className} />,
+    css: <img src={cssIcon} alt="" aria-hidden="true" className={className} />,
+    javascript: <img src={javascriptIcon} alt="" aria-hidden="true" className={className} />,
+    wordpress: <img src={wordpressIcon} alt="" aria-hidden="true" className={className} />,
+    shopify: <img src={shopifyIcon} alt="" aria-hidden="true" className={className} />,
+    woocommerce: <img src={woocommerceIcon} alt="" aria-hidden="true" className={className} />,
+    elementor: <img src={elementorIcon} alt="" aria-hidden="true" className={className} />,
   };
   return icons[name] ?? <Code2 className={className} />;
 }
